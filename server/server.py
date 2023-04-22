24745e12a69d386dee6a453b3ce86e1fc3ea361f
Marcus Bakke <marcusabakke@gmail.com>
2023-04-21 19:31:13 -0700

    Cleanup and comment python code. Small update to README

--- server/server.py
+++ server/server.py
@@ -12,52 +12,86 @@ from lsprotocol.types import (
     SemanticTokensParams,
     SemanticTokens,
 )
+
 from utils.read_docs import get_docs
 
 class NastranLanguageServer(LanguageServer):
+    """Subclass of pygls LanguageServer"""
 
-    CONFIGURATION_SECTION = 'nastranServer'
+    CONFIGURATION_SECTION = "nastranServer"
 
     def __init__(self, *args):
         super().__init__(*args)
 
+# Initialize server class
 server = NastranLanguageServer("NastranLanguageServer", "v0.1")
 
 def find_section(lines: list, section: str) -> Optional[int]:
+    """Finds line with given section divider token.
+
+    Args:
+        lines (list): List of all lines as strings in current file
+        section (str): Section divider token
+
+    Returns:
+        Optional[int]: Index of section divider if found otherwise None
+    """
     ind = [i for i, line in enumerate(lines) if line.lstrip().upper().startswith(section)]
     return ind[0] if ind else None
 
 def detect_section(line: int, ind: dict) -> str:
+    """Determines the section the provided line is in
+
+    Args:
+        line (int): Index of the current line
+        ind (dict): Dictionary containing all section divider indeces found
+
+    Returns:
+        str: Section the current line is in
+    """
     # If CEND and BEGIN BULK exist in file, assume at least EXEC,
     # CASE, and BULK sections exist.
-    if ind['CEND'] and ind['BEGIN BULK']:
+    if ind["CEND"] and ind["BEGIN BULK"]:
         # Detect DMAP section
-        if ind['ALTER'] and ind['ENDALTER']:
-            if line > ind['ALTER'] and line < ind['ENDALTER']:
-                return 'DMAP'
+        if ind["ALTER"] and ind["ENDALTER"]:
+            if line > ind["ALTER"] and line < ind["ENDALTER"]:
+                return "DMAP"
         # Detect EXEC section
-        if line <= ind['CEND']:
-            return 'EXEC'
+        if line <= ind["CEND"]:
+            return "EXEC"
         # Detect SUBS section
-        if ind['SUBSTRUCTURE'] and ind['ENDSUBS']:
-            if line >= ind['SUBSTRUCTURE'] and line <= ind['ENDSUBS']:
-                return 'SUBS'
+        if ind["SUBSTRUCTURE"] and ind["ENDSUBS"]:
+            if line >= ind["SUBSTRUCTURE"] and line <= ind["ENDSUBS"]:
+                return "SUBS"
         # Detect CASE section
-        if line <= ind['BEGIN BULK'] and line >= ind['CEND']:
-            return 'CASE'
+        if line <= ind["BEGIN BULK"] and line >= ind["CEND"]:
+            return "CASE"
     # Assume BULK section
-    return 'BULK'
+    return "BULK"
 
 @server.feature(TEXT_DOCUMENT_HOVER)
-async def hovers(ls: NastranLanguageServer, params: HoverParams) -> Optional[Hover]:
-    # ls.show_message(f"file: {params.text_document.uri}; line: {params.position.line}; character: {params.position.character}")
+async def hovers(params: HoverParams) -> Optional[Hover]:
+    """Hover provider
+
+    Args:
+        params (HoverParams): Hover parameters from VS Code client
+
+    Returns:
+        Optional[Hover]: Returns Hover class if documentation found else None
+    """
+    # Acquire current line
     doc = server.workspace.get_document(params.text_document.uri)
     orig_line = doc.lines[params.position.line]
+    # Strip leading white space and grab first 8 characters
+    # TODO: Change logic such that it grabs more than 8 characters?
     card = orig_line.lstrip()[:8]
-    for char in [',', '*', ' ', '=', '\n', '(']:
+    # If special character detected, strip everything after that character
+    for char in [",", "*", " ", "=", "\n", "("]:
         if char in card:
             card = card.split(char)[0]
-    if '$' not in card or card != '':
+    # If card is not blank and not a comment, find hover text
+    if "$" not in card or card != "":
+        # Only provide hover if cursor is near or on top of card
         logic = (params.position.character >= orig_line.index(card))
         logic = logic and (params.position.character  <= orig_line.index(card)+len(card))
         if logic:
@@ -80,8 +114,6 @@ async def hovers(ls: NastranLanguageServer, params: HoverParams) -> Optional[Hov
             return Hover(contents=contents)
     return None
 
-# Derived from:
-# https://github.com/openlawlibrary/pygls/blob/master/examples/json-vscode-extension/server/server.py
 @server.feature(
     TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
     SemanticTokensLegend(
@@ -89,15 +121,21 @@ async def hovers(ls: NastranLanguageServer, params: HoverParams) -> Optional[Hov
         token_modifiers=[],
     )
 )
-def semantic_tokens(ls: NastranLanguageServer, params: SemanticTokensParams):
-    """Used to signal to the client that we support semantic tokens."""
+def semantic_tokens(params: SemanticTokensParams) -> SemanticTokens:
+    """Semantic token provider
 
-    uri = params.text_document.uri
-    doc = ls.workspace.get_document(uri)
+    Args:
+        params (SemanticTokensParams): Semantic token params from extension
+
+    Returns:
+        SemanticTokens: Semantic token type
+    """
+    # Acquire document
+    doc = server.workspace.get_document(params.text_document.uri)
     
+    # Initialize counters and SemanticToken data
     last_line = 0
     last_start = 0
-    
     data = []
     
     # Find section designators
@@ -110,18 +148,22 @@ def semantic_tokens(ls: NastranLanguageServer, params: SemanticTokensParams):
         "ENDSUBS": find_section(doc.lines, "ENDSUBS"),
         "BEGIN BULK": find_section(doc.lines, "BEGIN BULK"),
     }
+    # For each line in the document...
     for lineno, line in enumerate(doc.lines):
+        # Determine the section of current line
         section = detect_section(lineno, ind)
         last_start = 0
-        if section == 'BULK' and '$' not in line.lstrip() and ',' not in line:
-            if '*' in line:
-                n = 16
-            else:
-                n =  8
+        # Process the BULK section
+        # Ignore comments and lines with free format (comma separated)
+        if section == "BULK" and "$" not in line.lstrip() and "," not in line:
+            # Determine if long or short format
+            n = 16 if "*" in line else 8
+            # Split the line by fields
             line_by_fields = [line[i:i+n] for i in range(0, len(line), n)]
             for i, _ in enumerate(line_by_fields[2::2]):
                 start = 2*n*(i)+n+8
                 end = start + n
+                # Save SemanticToken data
                 data += [
                     (lineno - last_line),
                     (start - last_start),
@@ -132,10 +174,15 @@ def semantic_tokens(ls: NastranLanguageServer, params: SemanticTokensParams):
                 last_line = lineno
                 last_start = start
                 
-        if section == 'SUBS':
-            if 'ENDSUBS' not in line and 'SUBSTRUCTURE' not in line:
+        # Process the SUBSTRUCTURE section
+        if section == "SUBS":
+            # Skip ENDSUBS and SUBTRUCTURE cards
+            if "ENDSUBS" not in line and "SUBSTRUCTURE" not in line:
+                # Find start of SUBTRUCTURE card
                 start = len(line) - len(line.lstrip())
-                end = min([line.lstrip().index(char) for char in ['\n', ' ', '=', '('] if char in line.lstrip()])+start
+                # Find end of SUBTRUCTURE card
+                end = min([line.lstrip().index(char) for char in ["\n", " ", "=", "("] if char in line.lstrip()])+start
+                # Save SemanticToken data
                 data += [
                     (lineno - last_line),
                     (start - last_start),
@@ -146,11 +193,16 @@ def semantic_tokens(ls: NastranLanguageServer, params: SemanticTokensParams):
                 last_line = lineno
                 last_start = start
         
-        if section == 'DMAP':
-            if line.lstrip()[0] != '$':
-                if 'ALTER' not in line:
+        # Process DMAP section
+        if section == "DMAP":
+            # Skip comments and ALTER lines
+            if line.lstrip()[0] != "$":
+                if "ALTER" not in line:
+                    # Find end of DMAP card
                     start = len(line) - len(line.lstrip())
-                    end = min([line.lstrip().index(char) for char in ['\n', ' ', '=', '('] if char in line.lstrip()])+start
+                    # Find end of DMAP card
+                    end = min([line.lstrip().index(char) for char in ["\n", " ", "=", "("] if char in line.lstrip()])+start
+                    # Save SemanticToken data
                     data += [
                         (lineno - last_line),
                         (start - last_start),
@@ -163,4 +215,5 @@ def semantic_tokens(ls: NastranLanguageServer, params: SemanticTokensParams):
 
     return SemanticTokens(data=data)
 
+# Start server
 server.start_io()
--- utils/read_docs.py
+++ utils/read_docs.py
@@ -1,55 +1,76 @@
 import os
 
 def get_docs(card: str, section='') -> str:
+    """Retrieves documentation for given Nastran entry and specified section.
+
+    Args:
+        card (str): Nastran entry to provide documentation for
+        section (str, optional): Section the entry belongs to to specifically
+                                 search. Defaults to ''.
+
+    Returns:
+        str: Documentation for Nastran entry
+    """
+    # If section provided, search that section
     if section:
         out = read_docs(card, section)
     else:
-        for section in ['BULK', 'CASE', 'EXEC', 'SUBS', 'DMAP', 'PLOT']:
+        # If section not provided, search in this order till entry is found
+        for section in ["BULK", "CASE", "EXEC", "SUBS", "DMAP", "PLOT"]:
             out = read_docs(card, section)
             if out:
                 break
+    # If documentation found, convert to markdown otherwise return empty string
     return convert_to_markdown(out) if out else out
 
 def read_docs(card: str, section: str) -> str:
+    """Parse NASTRAN-95 documentation given Nastran entry and section.
+
+    Args:
+        card (str): Nastran entry to provide documentation for
+        section (str): Section the entry belongs to to specifically
+                       search.
+
+    Returns:
+        str: Documentation for Nastran entry
+    """
     # Ignore hover for comment
-    if card == '$':
+    if card == "$":
         return ''
     # Read raw NASTRAN-95 documentation
-    with open(file=os.path.join('utils', 'um', section+'.TXT'), mode='r') as f:
+    with open(file=os.path.join("utils", "um", f"{section}.TXT"), mode="r") as f:
         lines = f.read()
     # Remove unknown characters
-    # lines = re.sub('[^'+string.printable+']+', ' ', lines)
-    lines = lines.replace('Ä','─')
-    lines = lines.replace('Å','┼')
-    lines = lines.replace('Â','┬')
-    lines = lines.replace('³','│').replace('º','│')
-    lines = lines.replace('Á','┴')
-    lines = lines.replace('Ú','┌').replace('É','┌')
-    lines = lines.replace('Ã','├')
-    lines = lines.replace('À','└').replace('È','└')
-    lines = lines.replace('¿','┐').replace('»','┐')
-    lines = lines.replace('´','┤')
-    lines = lines.replace('Ù','┘').replace('¼','┘')
-    lines = lines.replace('é', 'θ')
-    lines = lines.replace('è', 'φ').replace('í', 'φ')
-    lines = lines.replace('à', 'α')
+    lines = lines.replace("Ä","─")
+    lines = lines.replace("Å","┼")
+    lines = lines.replace("Â","┬")
+    lines = lines.replace("³","│").replace("º","│")
+    lines = lines.replace("Á","┴")
+    lines = lines.replace("Ú","┌").replace("É","┌")
+    lines = lines.replace("Ã","├")
+    lines = lines.replace("À","└").replace("È","└")
+    lines = lines.replace("¿","┐").replace("»","┐")
+    lines = lines.replace("´","┤")
+    lines = lines.replace("Ù","┘").replace("¼","┘")
+    lines = lines.replace("é", "θ")
+    lines = lines.replace("è", "φ").replace("í", "φ")
+    lines = lines.replace("à", "α")
     # Split by =PAGE=
-    lines = lines.split('=PAGE=\n')
+    lines = lines.split("=PAGE=\n")
     # Find index of desired card
     ind = [i for i, line in enumerate(lines) if line.startswith(card.upper())]
     # If not found, try trimming last char
     # (i.e. PDUM1 needs to use PDUMi documetation -> search for PDUM)
     if not ind:
         ind = [i for i, line in enumerate(lines) if line.startswith(card.upper()[:-1])]
+    # TODO: Add logic to search all found cards and return the closest match
+    #       instead of simply the first match
     return lines[ind[0]] if ind else ''
 
 def convert_to_markdown(lines: str) -> str:
-    # lines = lines.split('\n')
-    # lines = [f"```nastran\n{lines[0]}\n```"]+["```text"]+lines[1:]+["```"]
-    # lines = '\n'.join(lines)
-    lines = f"```text\n{lines}```"
-    return lines if lines else ''
+    return f"```text\n{lines}```"
 
 if __name__ == "__main__":
+    # Used for debugging
     os.chdir('..')
     print(get_docs('ALTER'))
