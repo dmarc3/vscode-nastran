4864b84bdc087f9bb36643eadc287aa27e0a9c1b
Marcus Bakke <marcusabakke@gmail.com>
2023-04-25 22:34:08 -0700

    Add build command / button. Add command to open file on tree item selection

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -43,7 +43,7 @@ function startLangServer(
 
 export class TreeDataProvider implements vscode.TreeDataProvider<IncludeFile> {
 
-    constructor(private root: string) {}
+    constructor() {}
   
     getTreeItem(element: IncludeFile): vscode.TreeItem {
         return element;
@@ -69,7 +69,6 @@ export class TreeDataProvider implements vscode.TreeDataProvider<IncludeFile> {
     }
 
     private getIncludes(element: IncludeFile): IncludeFile[] {
-        console.log(element.origin)
         const lines = fs.readFileSync(element.origin).toString().split("\n");
         var includes = []
         for (var line of lines) {
@@ -81,7 +80,6 @@ export class TreeDataProvider implements vscode.TreeDataProvider<IncludeFile> {
                     line = line.split('"')[1]
                 }
                 const fileName = path.join(path.dirname(element.origin), line)
-                console.log('  '+fileName)
                 if (this.hasIncludes(fileName)) {
                     includes.push(new IncludeFile(line, vscode.TreeItemCollapsibleState.Expanded, fileName))
                 } else {
@@ -92,23 +90,13 @@ export class TreeDataProvider implements vscode.TreeDataProvider<IncludeFile> {
         return includes
     }
 
-    //     // create
-    //     const tree = vscode.window.createTreeView('includeHierarchy', options);
-    //     const root = path.dirname(vscode.window.activeTextEditor.document.fileName)
-        
-    //     // Event for tree selection
-    //     tree.onDidChangeSelection(e => {
-    //         const relpath = path.join(root, String(e.selection[0].label));
-    //         const uri = vscode.Uri.file(relpath);
-    //         vscode.commands.executeCommand(
-    //             'vscode.open',
-    //             uri
-    //         )
-    //     });
+    private _onDidChangeTreeData: vscode.EventEmitter<IncludeFile | undefined | null | void> = new vscode.EventEmitter<IncludeFile | undefined | null | void>();
+    readonly onDidChangeTreeData: vscode.Event<IncludeFile | undefined | null | void> = this._onDidChangeTreeData.event;
+  
+    refresh(): void {
+      this._onDidChangeTreeData.fire();
+    }
 
-    //     // subscribe
-    //     context.subscriptions.push(tree);
-    // }
 }
 
 export function activate(context: vscode.ExtensionContext): void {
@@ -125,18 +113,15 @@ export function activate(context: vscode.ExtensionContext): void {
     client = startLangServer(pythonPath, ["-m", "server.server"], cwd);
     context.subscriptions.push(client.start());
     // Register Tree View
-    const root = vscode.window.activeTextEditor.document.fileName
+    const includeHierarchyProvider = new TreeDataProvider();
     vscode.window.registerTreeDataProvider(
         'includeHierarchy',
-        new TreeDataProvider(root)
+        includeHierarchyProvider
     );
-    vscode.commands.registerCommand("includeHierarchy.selectNode", (item:vscode.TreeItem) => {
-        // console.log(item.label);
-    });
     // Register Commands
-    vscode.commands.registerCommand('extension.buildIncludeTree', () => {
-        // console.log('test')
-    });
+    vscode.commands.registerCommand('includeHierarchy.buildHierarchy', () =>
+        includeHierarchyProvider.refresh()
+    );
 }
 
 export function deactivate(): Thenable<void> {
@@ -152,8 +137,13 @@ class IncludeFile extends vscode.TreeItem {
       super(label, collapsibleState);
       this.origin = this.origin;
     }
+    command = {
+        "title": "Open Include File",
+        "command": "vscode.open",
+        "arguments": [vscode.Uri.file(this.origin)]
+    }
     iconPath = {
-      light: path.join(__filename, '..', '..', 'icons', 'nasa-light.png'),
-      dark: path.join(__filename, '..', '..', 'icons', 'nasa-dark.png')
+      light: path.join(__filename, '..', '..', '..', 'resources', 'light', 'mesh.png'),
+      dark: path.join(__filename, '..', '..', '..', 'resources', 'dark', 'mesh.png')
     };
   }
